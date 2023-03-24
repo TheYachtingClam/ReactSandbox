b61581ad23a19c0ec7289231669a9ee7d65b91c2
Nicholas Mayne <nick@modelrocket.io>
2023-03-24 15:37:55 -0700

    style update

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import { NameList } from "./components/NameList";
+//import { NameList } from "./components/NameList";
 // import { ClickHandler } from "./components/ClickHandler";
 // import { ParentComponent } from "./components/ParentComponent";
 //import { UserGreeting } from "./components/UserGreeting";
 // import { Greet } from "./components/Greet";
 //import { Message } from "./components/Message";
+import { Stylesheet } from "./components/Stylesheet";
 
 function App() {
     return (
         <div className="App">
-            <NameList />
+            <Stylesheet />
         </div>
     );
 }
