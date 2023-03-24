73f6229e5e2c6cc4249602d658d7abbb962b54aa
Nicholas Mayne <nick@modelrocket.io>
2023-03-24 14:34:38 -0700

    Render list

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
+import { NameList } from "./components/NameList";
 // import { ClickHandler } from "./components/ClickHandler";
 // import { ParentComponent } from "./components/ParentComponent";
-import { UserGreeting } from "./components/UserGreeting";
+//import { UserGreeting } from "./components/UserGreeting";
 // import { Greet } from "./components/Greet";
 //import { Message } from "./components/Message";
 
 function App() {
     return (
         <div className="App">
-            <UserGreeting />
+            <NameList />
         </div>
     );
 }
