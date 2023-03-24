19280b8a8b7958df515605a8593b99df9a85b01b
Nicholas Mayne <nick@modelrocket.io>
2023-03-24 14:30:11 -0700

    Variable Rendering

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 // import { ClickHandler } from "./components/ClickHandler";
-import { ParentComponent } from "./components/ParentComponent";
+// import { ParentComponent } from "./components/ParentComponent";
+import { UserGreeting } from "./components/UserGreeting";
 // import { Greet } from "./components/Greet";
 //import { Message } from "./components/Message";
 
 function App() {
     return (
         <div className="App">
-            <ParentComponent />
+            <UserGreeting />
         </div>
     );
 }
