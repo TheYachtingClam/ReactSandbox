4935c90ce8ac7865ec70d18202de3db4a0062836
Nicholas Mayne <nick@modelrocket.io>
2023-03-24 14:16:20 -0700

    Parent callbacks

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,20 +1,13 @@
 import "./App.css";
-import { ClickHandler } from "./components/ClickHandler";
+// import { ClickHandler } from "./components/ClickHandler";
+import { ParentComponent } from "./components/ParentComponent";
 // import { Greet } from "./components/Greet";
 //import { Message } from "./components/Message";
 
 function App() {
     return (
         <div className="App">
-            {/* <Greet name="Bruce" heroName="Batman" />
-            <Greet name="Clark" heroName="Superman">
-                <button>Action</button>
-            </Greet>
-            <Greet name="Diana" heroName="Wonder Woman">
-                <p>This is children props</p>
-            </Greet> */}
-            {/* <Message /> */}
-            <ClickHandler />
+            <ParentComponent />
         </div>
     );
 }
